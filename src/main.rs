c8f07d790e4decea512fcd7b564254ccc22f04d2
Michael Taggart <mtaggart@taggart-tech.com>
2024-02-25 00:24:31 -0800

    Add directory handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env::args;
 use std::fs;
+use std::path::{Path, PathBuf};
 
 // Max Filesize of 2GB
 const MAX_FILESIZE: u64 = 2147483648;
@@ -7,57 +8,86 @@ const MAX_FILESIZE: u64 = 2147483648;
 // Chunk size for entropy analysis
 const MAX_ENTROPY_CHUNK: usize = 2560000;
 
-fn calculate_entropy(filename: &str) -> Result<f64, String> {
+// const MAGIC_BYTES_LEN: usize = 4;
+
+// const ELF_MAGIC: &str = "7f454c46";
+
+///
+/// Calculate the entropy of a single file.
+///
+/// Uses [MAX_FILESIZE] as a hard limit
+/// for what will be scanned.
+///
+fn calculate_entropy(path: &Path) -> Result<f64, String> {
+    // Announce intentions
+    println!("Scanning {:?}", &path);
+
     // Check for ELF
 
     // Check max size
-    if let Ok(metadata) = fs::metadata(filename) {
+    if let Ok(metadata) = fs::metadata(path) {
         if metadata.len() > MAX_FILESIZE {
             return Err("File too large!".to_string());
         }
-    
-        if let Ok(file_bytes) = fs::read(filename) {
+
+        if let Ok(file_bytes) = fs::read(path) {
             let mut entropy = 0.0f64;
             for chunk in file_bytes.chunks(MAX_ENTROPY_CHUNK) {
                 let mut frequency: [u32; 256] = [0; 256];
                 let mut total_bytes = 0;
-        
+
                 for byte in chunk {
                     frequency[*byte as usize] += 1;
                     total_bytes += 1;
                 }
-        
-        
+
                 for count in frequency.iter() {
                     if *count == 0 {
                         continue;
                     }
-        
+
                     let p = *count as f64 / total_bytes as f64;
                     entropy -= p * p.log2();
                 }
             }
             Ok(entropy)
-    
         } else {
             Err("Couldn't read file!".to_string())
         }
-
     } else {
         Err("Couldn't get file metadata!".to_string())
     }
-
-
-
 }
 
-fn main() -> Result<(),String>{
-    if let Some(filename) = args().nth(1) {
-        println!("Scanning {filename}");
-        let entropy = calculate_entropy(&filename)?;
-        println!("Entropy of {filename}: {entropy}");
+///
+/// Recursively scans the target path to collect all files to scan
+///
+fn collect_targets(parent_path: PathBuf) -> Vec<PathBuf> {
+    let mut targets = Vec::new();
+    if let Ok(dir) = fs::read_dir(parent_path) {
+        for entry in dir {
+            if let Ok(entry) = entry {
+                let path = entry.path();
+                if path.is_dir() {
+                    targets.extend(collect_targets(path));
+                } else {
+                    targets.push(path);
+                }
+            }
+        }
+    }
+    targets
+}
+
+fn main() -> Result<(), String> {
+    if let Some(target) = args().nth(1) {
+        let targets = collect_targets(PathBuf::from(target.to_owned()));
+        for target in targets {
+            let entropy = calculate_entropy(&PathBuf::from(target.to_owned()))?;
+            println!("{target:?}: {entropy}");
+        }
         Ok(())
     } else {
-        panic!("No filename provided!")
+        panic!("No path provided!")
     }
 }
